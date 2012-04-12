d130b1bf40a4dae0e08f31d62dce07fe5553fc4a
Anna Cruz <annacruz@koshtech.com>
2012-04-12 14:28:51 -0300

    #1867: Demais atividades de construção do aplicativo

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
   :domain               => "gmail.com",  
-  :user_name            => "anna.cruz@gmail.com",  
-  :password             => "graci150103",  
+  :user_name            => user,  
+  :password             => password,  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }  
